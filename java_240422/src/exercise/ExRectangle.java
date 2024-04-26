8e3ba3124d3d1d508933a1a395662e9515763218
wander0000 <yeonjuseo712@gmail.com>
2024-04-26 17:47:41 +0900

    실습예제2만

--- java_240422/src/exercise/ExRectangle.java
+++ java_240422/src/exercise/ExRectangle.java
@@ -7,13 +7,14 @@ public class ExRectangle {
 		
 		System.out.print("가로, 세로 길이를 입력하세요>>");
 		Scanner scan = new Scanner(System.in);
-		
+//		첫버째 정수를 가로 변수로 입력받음
 		int width= scan.nextInt();
+//		첫버째 정수를 세로 변수로 입력받음
 		int height= scan.nextInt();
-		int area=width*height;
+		int RectangleArea=width*height;
 		
-		System.out.println("사각형의 면적은 "+area+"입니다.");
+		System.out.println("사각형의 면적은 "+RectangleArea+"입니다.");
 		scan.close();
 	
-}
+	}
 }
